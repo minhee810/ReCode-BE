ee1f204fe8872df311f20546dfac398e9d219fa9
Chan <chc6823@naver.com>
2023-11-10 11:32:09 +0900

    study create-1(not completed)

---
+++ src/main/java/com/abo2/recode/domain/StudyRoom/StudyRepository.java
@@ -0,0 +1,8 @@
+package com.abo2.recode.domain.StudyRoom;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface StudyRepository extends JpaRepository<StudyRoom,Long> {
+
+}
