9f816f9cd9f886fa57e7fb6d27481d7f756325d5
dahyunhan <dollae7020@gmail.com>
2023-11-15 14:23:11 +0900

    move file

--- src/main/java/com/abo2/recode/repository/QnaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abo2.recode.repository;
-
-import com.abo2.recode.entity.Qna;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QnaRepository extends JpaRepository<Qna,Long> {
-}
