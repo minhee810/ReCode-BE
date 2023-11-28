08267cbf93273a036cbc20a8ab2a6b124ec2332d
Chan <chc6823@naver.com>
2023-11-28 09:57:03 +0900

    temp

--- src/main/java/com/abo2/recode/domain/studyroom/StudyRoomRepository.java
+++ src/main/java/com/abo2/recode/domain/studyroom/StudyRoomRepository.java
@@ -1,7 +1,6 @@
 package com.abo2.recode.domain.studyroom;
 
 import com.abo2.recode.domain.user.User;
-import com.abo2.recode.dto.study.StudyResDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,8 +13,14 @@ public interface StudyRoomRepository extends JpaRepository<StudyRoom,Long> {
 
     Optional<StudyRoom> findById(Long Id);
 
+    // user_id,study_id를 기반으로 먼저 유저가 이미 가입했거나 가입한 상태인지 체크
+    @Query(value = "SELECT study_member_id " +
+            "FROM Study_Member WHERE study_room_id=:study_room_id AND user_id=:user_id",nativeQuery = true)
+    Long findIdByuser_idAndStudy_id(@Param(value = "study_room_id")Long study_room_id,
+                                    @Param(value = "user_id")Long user_id);
+
     @Query(value = "SELECT created_by FROM Study_Room s WHERE s.study_room_id=:study_room_id",nativeQuery = true)
-    Long findCreated_byBYstudy_id(@Param("study_room_id") Long study_room_id);
+    Long findCreated_byBystudy_id(@Param("study_room_id") Long study_room_id);
 
     @Query("SELECT sr FROM StudyRoom sr LEFT JOIN FETCH sr.master LEFT JOIN FETCH sr.studySkills WHERE sr.id = :id")
     Optional<StudyRoom> findWithMasterAndSkillsById(@Param("id") Long id);
