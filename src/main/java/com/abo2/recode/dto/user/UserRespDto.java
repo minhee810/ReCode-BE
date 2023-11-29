78ee3fa92aa419cf399d9e1b95cb7702e06f214a
bearkuang <137677440+bearkuang@users.noreply.github.com>
2023-11-29 15:38:02 +0900

    스터디에 가입되어 있지 않은 유저가 탈퇴 되지 않는 문제 해결

--- src/main/java/com/abo2/recode/dto/user/UserRespDto.java
+++ src/main/java/com/abo2/recode/dto/user/UserRespDto.java
@@ -90,12 +90,12 @@ public class UserRespDto {
 
     @Getter
     @Setter
-    public static class getUserInfoDto{
+    public static class GetUserInfoDto{
         private String username;
         private String nickname;
         private String email;
 
-        public getUserInfoDto(User user){
+        public GetUserInfoDto(User user){
             this.username = user.getUsername();
             this.nickname = user.getNickname();
             this.email = user.getEmail();
@@ -104,10 +104,10 @@ public class UserRespDto {
 
     @Getter
     @Setter
-    public static class changePasswordRespDto{
+    public static class ChangePasswordRespDto{
         private String password;
 
-        public changePasswordRespDto(User user){
+        public ChangePasswordRespDto(User user){
             this.password = user.getPassword();
         }
     }
