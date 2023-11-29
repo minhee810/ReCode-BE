9712f2877d15fdbff2cbaf3dda11aa9f7b57ab9e
dahyunhan <dollae7020@gmail.com>
2023-11-29 12:52:26 +0900

    qnaReply CRUD 완료

--- src/main/java/com/abo2/recode/controller/QnaReplyController.java
+++ src/main/java/com/abo2/recode/controller/QnaReplyController.java
@@ -3,6 +3,7 @@ package com.abo2.recode.controller;
 import com.abo2.recode.config.auth.LoginUser;
 import com.abo2.recode.domain.qna.Qna;
 import com.abo2.recode.domain.qna.QnaReply;
+import com.abo2.recode.domain.qna.QnaRepository;
 import com.abo2.recode.domain.user.User;
 import com.abo2.recode.domain.user.UserEnum;
 import com.abo2.recode.domain.user.UserRepository;
@@ -24,14 +25,18 @@ public class QnaReplyController {
 
     private final QnaReplyService qnaReplyService;
     private final UserRepository userRepository;
+    private final QnaRepository qnaRepository;
 
+    //Qna 댓글 생성
     @PostMapping("/qna-reply/{id}")
-    public ResponseEntity<?> postQnaReply(@AuthenticationPrincipal LoginUser loginUser, @PathVariable Long id, QnaReplyDTO qnaReplyDTO, Qna qna) {
+    public ResponseEntity<?> postQnaReply(@AuthenticationPrincipal LoginUser loginUser, @PathVariable Long id, @RequestBody QnaReplyDTO qnaReplyDTO) {
         qnaReplyDTO.setUser_id(loginUser.getUser().getId());
+
         User user = userRepository.findById(loginUser.getUser().getId()).orElseThrow();
+        Qna qna = qnaRepository.findById(id).orElseThrow();
 
+        if (user.getId() == qna.getUser_id().getId() || user.getRole() == UserEnum.ADMIN) {
 
-        if (qna.getUser_id() == user || user.getRole() == UserEnum.ADMIN) {
             qnaReplyDTO.setQna_id(id);
             qnaReplyService.postQnaReply(qnaReplyDTO);
 
@@ -41,41 +46,48 @@ public class QnaReplyController {
         return new ResponseEntity<>(new ResponseDto<>(-1, "권한 없음", null), HttpStatus.BAD_REQUEST);
     }
 
+    //Qna 댓글 조회
     @GetMapping("/qna-reply/{id}")
     public ResponseEntity<?> qnaReplys(@AuthenticationPrincipal LoginUser loginUser, @PathVariable Long id, QnaReplyDTO qnaReplyDTO) {
         qnaReplyDTO.setUser_id(loginUser.getUser().getId());
-//        Qna qna = qnaRepository.findById(id).orElseThrow();
+
         qnaReplyDTO.setQna_id(id);
         List<QnaReply> qnaReplies = qnaReplyService.qnaReplies();
 
         return new ResponseEntity<>(new ResponseDto<>(1, "Qna 댓글 조회 성공", qnaReplies), HttpStatus.OK);
     }
 
+    //Qna 댓글 수정
     @PutMapping("qna-reply/{id}/{reply_id}")
-    public ResponseEntity<?> qnaReplyModify(@AuthenticationPrincipal LoginUser loginUser, @PathVariable Long id, @PathVariable Long reply_id, QnaReplyDTO qnaReplyDTO, Qna qna) {
+    public ResponseEntity<?> qnaReplyModify(@AuthenticationPrincipal LoginUser loginUser, @PathVariable Long id, @PathVariable Long reply_id, @RequestBody QnaReplyDTO qnaReplyDTO) {
         qnaReplyDTO.setUser_id(loginUser.getUser().getId());
+
         User user = userRepository.findById(loginUser.getUser().getId()).orElseThrow();
+        Qna qna = qnaRepository.findById(reply_id).orElseThrow();
 
-
-        if (qna.getUser_id() == user) {
+        if (qna.getUser_id().getId() == user.getId()) {
 
             qnaReplyDTO.setQna_id(id);
             qnaReplyService.qnaReplyModify(qnaReplyDTO, reply_id);
+
             return new ResponseEntity<>(new ResponseDto<>(1, "Qna 댓글 수정 성공", qnaReplyDTO), HttpStatus.OK);
         }
         return new ResponseEntity<>(new ResponseDto<>(-1, "권한 없음", null), HttpStatus.BAD_REQUEST);
     }
 
+    //Qna 댓글 삭제 (관리자 권한)
     @DeleteMapping("qna-reply/{id}/{reply_id}")
     public ResponseEntity<?> qnaReplyDelete(@AuthenticationPrincipal LoginUser loginUser, @PathVariable Long id, @PathVariable Long reply_id, QnaReplyDTO qnaReplyDTO, Qna qna) {
         qnaReplyDTO.setUser_id(loginUser.getUser().getId());
+
         User user = userRepository.findById(loginUser.getUser().getId()).orElseThrow();
 
         if (qna.getUser_id() == user || user.getRole() == UserEnum.ADMIN) {
+
             qnaReplyDTO.setQna_id(id);
             qnaReplyService.qnaReplyDelete(reply_id);
-            return new ResponseEntity<>(new ResponseDto<>(1, "Qna 댓글 삭제 성공", qnaReplyDTO), HttpStatus.OK);
 
+            return new ResponseEntity<>(new ResponseDto<>(1, "Qna 댓글 삭제 성공", qnaReplyDTO), HttpStatus.OK);
         }
         return new ResponseEntity<>(new ResponseDto<>(-1, "권한 없음", null), HttpStatus.BAD_REQUEST);
     }
