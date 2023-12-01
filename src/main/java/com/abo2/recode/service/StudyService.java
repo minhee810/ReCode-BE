c89872684f14d9a58e3e48a1898e0a267ff786ed
Chan <chc6823@naver.com>
2023-12-01 10:18:18 +0900

    스터디 탈퇴 수정

--- src/main/java/com/abo2/recode/service/StudyService.java
+++ src/main/java/com/abo2/recode/service/StudyService.java
@@ -359,7 +359,7 @@ public class StudyService {
         if (studyRoom.isPresent()) {
             StudyRoom room = studyRoom.get();
 
-            if (!room.getMaster().equals(userId)) {
+            if (! (room.getMaster().getId() == userId) ) {
                 studyMemberRepository.deleteByUserIdAndStudyRoomId(userId, study_room_id);
             } else {
                 throw new CustomApiException("스터디 장은 탈퇴가 불가능합니다. 권한을 양도한 후에 시도해 주시기 바랍니다.");
