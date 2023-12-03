bf9f4436dea13cf532e3224ac84e20ae2dde976b
백승주 <137259484+Oriduckquak@users.noreply.github.com>
2023-12-03 17:25:05 +0900

    userReqDto email 글짜 한도 증가, 스터디 신청, 승인 시 사용자에게 이메일 전공 구현

--- src/main/java/com/abo2/recode/controller/StudyMemberController.java
+++ src/main/java/com/abo2/recode/controller/StudyMemberController.java
@@ -1,9 +1,12 @@
 package com.abo2.recode.controller;
 
 import com.abo2.recode.config.auth.LoginUser;
+import com.abo2.recode.domain.user.User;
+import com.abo2.recode.domain.user.UserRepository;
 import com.abo2.recode.dto.ResponseDto;
 import com.abo2.recode.dto.study.StudyReqDto;
 import com.abo2.recode.dto.study.StudyResDto;
+import com.abo2.recode.service.EmailService;
 import com.abo2.recode.service.StudyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
+import java.util.Optional;
+
 @RestController
 @RequestMapping(value="/api")
 public class StudyMemberController {
@@ -21,9 +27,15 @@ public class StudyMemberController {
 
     private StudyService studyService;
 
+    private EmailService emailService;
+
+    private UserRepository userRepository;
+
     @Autowired
-    public StudyMemberController(StudyService studyService) {
+    public StudyMemberController(StudyService studyService, EmailService emailService, UserRepository userRepository) {
         this.studyService = studyService;
+        this.emailService = emailService;
+        this.userRepository = userRepository;
     }
 
     //스터디 조장의 스터디 조원 승인
@@ -33,12 +45,19 @@ public class StudyMemberController {
             @AuthenticationPrincipal LoginUser loginUser,
             @PathVariable(name = "study_id") Long study_id,
             @PathVariable(name="user_id") Long user_id
-    ){
+    ) throws MessagingException {
 
         System.out.println(study_id);
         System.out.println(user_id);
 
         StudyResDto.StudyMembershipResDto studyMembershipResDto;
+
+        Optional<User> userOpt = userRepository.findById(user_id);
+        if(!userOpt.isPresent()) {
+            return ResponseEntity.badRequest().body(new ResponseDto<>(-1, "유저를 찾을 수 없습니다.", null));
+        }
+
+        User user = userOpt.get();
 
         if (studyMembershipReqDto.getStatus().equals("Approved")){
             // 승인 된 경우
@@ -52,6 +71,8 @@ public class StudyMemberController {
                     .user_id(user_id)
                     .status("Approved")
                     .build();
+
+            emailService.sendApprovedEmail(study_id, user_id, user);
 
             return new ResponseEntity<>(new ResponseDto<>(1, "가입 신청이 승인되었습니다.",
                     studyMembershipResDto), HttpStatus.OK);
@@ -67,6 +88,8 @@ public class StudyMemberController {
                 .user_id(user_id)
                 .status("Rejected")
                 .build();
+
+        emailService.sendRejectedEmail(study_id, user_id, user);
 
         return new ResponseEntity<>(new ResponseDto<>(1, "가입 신청이 거부되었습니다.",
                 studyMembershipResDto), HttpStatus.OK);
--- src/main/java/com/abo2/recode/service/EmailService.java
+++ src/main/java/com/abo2/recode/service/EmailService.java
@@ -39,6 +39,60 @@ public class EmailService {
 
         helper.setText(msgg, true); // true indicates HTML content
         helper.setTo(user.getEmail());
+        helper.setSubject("Recode 스터디 신청에 관한 알림입니다.");
+        helper.setFrom("Recode");
+
+        javaMailSender.send(mimeMessage);
+    }
+
+    public void sendApprovedEmail(Long study_id, Long user_id, User user) throws MessagingException {
+        String url = "http://localhost:3000/login";
+
+        String msgg = "<div style='margin:20px;'>";
+        msgg += "<h1> 안녕하세요 Recode 입니다. </h1>";
+        msgg += "<br>";
+        msgg += "<p>신청하신 스터디에 가입이 승인되셨습니다.<p>";
+        msgg += "<br>";
+        msgg += "<p>Recode 에 접속하여 확인해주세요!<p>";
+        msgg += "<br>";
+        msgg += "<div align='center' style='border:1px solid black; font-family:verdana;'>";
+        msgg += "<h3 style='color:blue;'>Recode 로그인하러 가기</h3>";
+        msgg += "<div style='font-size:130%'>";
+        msgg += "<a href='" + url + "'>링크를 눌러 승인된 스터디를 확인하세요!</a></div><br/> ";
+        msgg += "</div>";
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+
+        helper.setText(msgg, true); // true indicates HTML content
+        helper.setTo(user.getEmail());
+        helper.setSubject("Recode 스터디 신청에 관한 알림입니다.");
+        helper.setFrom("Recode");
+
+        javaMailSender.send(mimeMessage);
+    }
+
+    public void sendRejectedEmail(Long study_id, Long user_id, User user) throws MessagingException {
+        String url = "http://localhost:3000/login";
+
+        String msgg = "<div style='margin:20px;'>";
+        msgg += "<h1> 안녕하세요 Recode 입니다. </h1>";
+        msgg += "<br>";
+        msgg += "<p>신청하신 스터디에 가입이 거절되셨습니다.<p>";
+        msgg += "<br>";
+        msgg += "<p>Recode 에 접속하여 확인해주세요!<p>";
+        msgg += "<br>";
+        msgg += "<div align='center' style='border:1px solid black; font-family:verdana;'>";
+        msgg += "<h3 style='color:blue;'>Recode 로그인하러 가기</h3>";
+        msgg += "<div style='font-size:130%'>";
+        msgg += "<a href='" + url + "'>링크를 눌러 다른 스터디도 둘러봐 주세요!</a></div><br/> ";
+        msgg += "</div>";
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+
+        helper.setText(msgg, true); // true indicates HTML content
+        helper.setTo(user.getEmail());
         helper.setSubject("Recode 비밀번호 변경 관련 인증 코드");
         helper.setFrom("Recode");
 
