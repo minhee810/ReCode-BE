6f07c739dd4641b99afe1731b6ab2c7a93d3c9e5
dollae <96718120+dahyunhan@users.noreply.github.com>
2023-12-04 18:28:39 +0900

    Update EmailConfig.java

--- src/main/java/com/abo2/recode/config/EmailConfig.java
+++ src/main/java/com/abo2/recode/config/EmailConfig.java
@@ -10,7 +10,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import java.util.Properties;
 
 @Configuration
-@PropertySource("file: /email.properties")
+@PropertySource("file:email.properties")
 public class EmailConfig {
 
     @Value("${mail.smtp.port}")
