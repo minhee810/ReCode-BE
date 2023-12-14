1101cbfef449e82801dee2a75e3c3f2f7d3e00a8
minhee <minhee810@naver.com>
2023-12-14 17:58:56 +0900

    브랜치 병합 전

---
+++ src/main/java/com/abo2/recode/service/FileUploadService.java
@@ -0,0 +1,15 @@
+package com.abo2.recode.service;
+
+import com.abo2.recode.dto.fileupload.FileResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class FileUploadService {
+
+
+//    public String upload(MultipartFile image) {
+//    }
+}
