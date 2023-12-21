eaa81150024eb93016206225bf5caff175a123e5
bearkuang <137677440+bearkuang@users.noreply.github.com>
2023-12-21 09:55:15 +0900

    StudyMembershipReqDto 수정

--- src/main/java/com/abo2/recode/dto/study/StudyReqDto.java
+++ src/main/java/com/abo2/recode/dto/study/StudyReqDto.java
@@ -161,7 +161,6 @@ public class StudyReqDto {
         @Size(min = 1, max = 50)
         private String status;
 
-        public StudyMembershipReqDto() {}
         @Builder
         public StudyMembershipReqDto(String status) {
             this.status = status;
